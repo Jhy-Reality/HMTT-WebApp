e73cde0daea557bc0f52f8641ac95491c564b62f
Jhy <1633643741@qq.com>
2019-11-29 16:47:16 +0800

    项目初始化构建

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入移动端Vant组件库
+import Vant from 'vant'
+// 引入组件库的CSS文件
+import 'vant/lib/index.css'
+// 注册Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,16 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 设置数据
   state: {
   },
+  // 修改数据
   mutations: {
   },
+  // 获取数据
   actions: {
-  },
-  modules: {
   }
+  /* // 将数据分模块化管理
+  modules: {
+  } */
 })
